cebce8605570d6f9dca7850a986f8cb93c87cab9
Kenneth <cheungkenneth-@hotmail.com>
2017-08-13 13:47:04 +0200

    :20170813

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
 
 gulp.task('default', function() {
 	console.log("Mr. Robot");
 });
 
+
 gulp.task('html', function() {
 	console.log("The Crown");
 });
 
+
 gulp.task('styles', function() {
-	console.log("Ghost in the Shell (2017)");
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
 });
 
 
@@ -19,7 +28,6 @@ gulp.task('watch', function() {
 	watch('./app/index.html', function() {
 		gulp.start('html')
 	});
-
 
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('styles')
